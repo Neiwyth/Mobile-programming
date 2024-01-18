d49a1fb7803365282abffe47372ba8d1f137936d
Neiwyth <user.example@mailing.com>
2024-01-18 12:24:59 +0200

    Test

--- Test/App.js
+++ Test/App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [msg, setMsg] = useState('');
+
+  const showAlert = () => {
+    Alert.alert('黒歴史 ' + msg);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={{ width: 200, borderColor: 'grey', borderWidth: 1 }} onChangeText={text => setMsg(text)} />
+      <Button title='Sure' onPress={showAlert} />
       <StatusBar style="auto" />
     </View>
   );
