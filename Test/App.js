aad7cd22bca0ce3ea448e71de9eefb50077a4667
Neiwyth <user.example@mailing.com>
2024-01-24 10:09:01 +0200

    Flatlist example

--- Test/App.js
+++ Test/App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Alert, Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View, FlatList, Text } from 'react-native';
 
 export default function App() {
 
-  const [msg, setMsg] = useState('');
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  const showAlert = () => {
-    Alert.alert('黒歴史 ' + msg);
+  const handleAdd = () => {
+    setTodos([...todos, todo]);
   }
 
+  const itemSeparator = () => {
+    return (<View style={{ height: 1, backgroundColor: 'black' }}></View>);
+  }
+
+  console.log(todos);
 
   return (
     <View style={styles.container}>
-      <TextInput style={{ width: 200, borderColor: 'grey', borderWidth: 1 }} onChangeText={text => setMsg(text)} />
-      <Button title='Sure' onPress={showAlert} />
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={{ fontSize: 12 }}
+          placeholder='Type todo'
+          onChangeText={text => setTodo(text)}
+          value={todo}
+        />
+        <Button title='Add' onPress={handleAdd} />
+      </View>
+      <View style={{ flex: 4 }}>
+        <FlatList
+          data={todos}
+          ItemSeparatorComponent={itemSeparator}
+          renderItem={({ item }) =>
+            <View style={styles.listItem}>
+              <Text style={{ fontSize: 13 }}>Todo: </Text>
+              <Text style={{ fontSize: 13 }}>{item}</Text>
+            </View>}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,8 +47,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 60,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  listItem: {
+    flexDirection: 'row',
+    width: 300,
+    padding: 10,
+    backgroundColor: 'lightblue',
   },
 });
